4a26ddf8efb170c938e2df6568b158306d0213b8
Mary Smith <mary.sunshine4@gmail.com>
2021-12-14 21:55:28 -0700

    readme is born

--- seed_database.py
+++ seed_database.py
@@ -11,11 +11,11 @@ model.connect_to_db(server.app)
 model.db.create_all()
 
 
-crud.create_customer('Customer', 'Try', '5551234567', '5551234568', 'customer@test.test')
-crud.create_customer('Another', 'Customer', '2385554444', None, None)
-crud.create_customer('Jane', 'Doe', '5678994444', None, None)
-crud.create_customer('Mary', 'Smith', '3333333333', None, None)
-crud.create_customer('Mary', 'Smith', '2222222222', None, None)
+# crud.create_customer('Customer', 'Try', '5551234567', '5551234568', 'customer@test.test')
+# crud.create_customer('Another', 'Customer', '2385554444', None, None)
+# crud.create_customer('Jane', 'Doe', '5678994444', None, None)
+crud.create_customer('Mary', 'Smith', '+13333333333', None, None)
+crud.create_customer('Mary', 'Smith', '+12222222222', None, None)
 
 # crud.create_appointment(1, 'hair', datetime(2021, 12, 2, 16, 30), datetime(2021, 12, 2), time(hour=16, minute=30), "US/Mountain", "0:45", 'woman cut', "Boo", "HI", "hello", True, False, False)
 # crud.create_appointment(2, 'hair', datetime(2021, 12, 3, 16, 30), datetime(2021, 12, 3), time(hour=16, minute=30), "US/Mountain", "0:30", 'man cut', "Boo", "HI", "hello", True, False, False)
